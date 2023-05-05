d2360c050a502fa72048ef7920d87278143b55d3
BONDY-Gaspard <116551040+BONDY-Gaspard@users.noreply.github.com>
2023-05-05 14:35:47 +0200

    Date décroissante

---
+++ backend.mjs
@@ -0,0 +1,15 @@
+import PocketBase from 'pocketbase' ;
+const pb = new PocketBase('http://127.0.0.1:8090') ;
+
+//await pb.collection('maison').getFullList() ;
+//const records = await pb.collection('maison').getFullList() ;
+
+try { const records = await pb.collection('maison').getFullList({ sort : '-created', }) ;
+    console.table(records) ;
+    } catch (e) {
+    console.error(e) ;
+    }
+
+//await pb.collection('maison').getFullList();
+//await pb.collection('maison').getOne('55wgbngz9010q42');
+//await pb.collection('maison').getFullList({ sort : 'date', });
