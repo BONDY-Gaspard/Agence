44463437a85704dedad679c401c913a3fa5a6d26
BONDY-Gaspard <116551040+BONDY-Gaspard@users.noreply.github.com>
2023-05-23 17:30:22 +0200

    DeleteAgentByID

--- backend.mjs
+++ backend.mjs
@@ -1,85 +1,18 @@
 import PocketBase from 'pocketbase';
 const pb = new PocketBase('http://127.0.0.1:8090');
 
- export async function allMaisons() {
-    const records = await pb.collection('maison').getFullList() ;
-    return records ;
-    } 
+export async function addNewMaison(newMaison) {
+    await pb.collection('maison').create(newMaison);
+}
 
-export async function OneID() {
-    const records = await pb.collection('maison').getOne('55wgbngz9010q42') ;
-    return records ;
-    }
+export async function addNewAgent(newAgent) {
+    await pb.collection('agent').create(newAgent);
+}
 
-export async function allMaisonsFavori() {
-    const records = await pb.collection('maison').getFullList({filter : "favori = TRUE", })
-    return records ;
-    }
+export async function DeleteMaisonById(RECORD_ID) {
+    await pb.collection('maison').delete(RECORD_ID) ;
+}
 
-export async function allMaisonsSorted() {
-    const records = await pb.collection('maison').getFullList({ sort: 'prix', })
-    return records ;
-    }
-
-export async function bySurface() {
-    const records = await pb.collection('maison').getFullList({filter : "surface >100", })
-    return records ;
-    }
-
-export async function surfaceORprice() {
-    const records = await pb.collection('maison').getFullList({filter : "surface >= 100 && prix <= 1000000" , })
-    return records ;
-    }
-
-export async function AgentID() {
-    const records = await pb.collection('agent').getOne('grlhec2g0wp03uz') ;
-    return records ;
-    }
-
-export async function allMaisonsAgents() {
-    const record = await pb.collection('maison').getFullList({
-        expand:'agent'
-    });
-    return record;
-    }
-
-export async function allMaisonsByAgentId(agentId) {
-    const agent = await getAgentById(agentId);
-        
-    if (!agent) {
-        throw new Error('Agent not found');
-    }
-        
-    const records = await pb.collection('maison').getFullList({ filter: `agents="${agentId}"` });
-    return records;
-    }
-    
-export async function allMaisonsByAgentName(nom){
-    const record = await pb.collection('agent').getFirstListItem(`nom ='${nom}'`, {expand: "maison(agent)"});
-    return record;
-    }
-        
-export async function allMaisonsSortedAgent() {
-    const records = await pb.collection('maison').getFullList({
-    expand:'agents',
-    sort: 'agents.nom'
-    });
-    return records;
-    }
-       
-export async function bySurfaceAgent(surface, agentId) {
-    const records = await pb.collection('maison').getFullList({
-    filter: `superficie > ${JSON.stringify(surface)} && agents.id = ${JSON.stringify(agentId)}`
-    });
-        
-    return records;
-    }
-      
-export async function maisonFavoriAgent(agentId) {
-    const records = await pb.collection('maison').getFullList({
-    filter: `agents.id = ${JSON.stringify(agentId)} && favori = true`
-    });
-        
-    return records;
-    }
-         
+export async function DeleteAgentById(RECORD_ID) {
+    await pb.collection('agent').delete(RECORD_ID) ;
+}
--- testBack.js
+++ testBack.js
@@ -1,100 +1,46 @@
-import { OneID, allMaisons, allMaisonsFavori, allMaisonsSorted, bySurface, surfaceORprice, AgentID, allMaisonsAgents, allMaisonsByAgentId, allMaisonsByAgentName, allMaisonsSortedAgent, bySurfaceAgent, maisonFavoriAgent } from './backend.mjs'
-
-
-/* try {
-    const records = await allMaisons() ;
-    console.log(JSON.stringify(records, null, 2)) ;
-    } catch (e) {
-    console.error(e) ;
-    } */
+import {addNewMaison,addNewAgent, DeleteMaisonById, DeleteAgentById } from './backend.mjs'
 
 /* try {
-    const record = await OneID();
-    console.log(JSON.stringify(record, null, 2));
-    } catch (e) {
-    console.error(e);
-    } */
+const newMaison ={
+    "nomMaison" : "MaisonMMI",
+    "prix" : 250000,
+    "nbChambres" : 4,
+    "nbSdb" : 2,
+    "adresse" : "Montbéliard",
+    "surface" : 123,
+    "favori" : true,
+    "agent" : "grlhec2g0wp03uz"
+};
+
+await addNewMaison(newMaison);
+} catch (e) {
+console.error(e);
+} */
 
 /* try {
-    const record = await allMaisonsFavori();
-    console.log(JSON.stringify(record, null, 2));
-    } catch (e) {
-    console.error(e);
-    } */
-
-/* try {
-    const record = await allMaisonsSorted();
-    console.log(JSON.stringify(record, null, 2));
-    } catch (e) {
-    console.error(e);
-    } */
-
-/* try {
-    const record = await bySurface();
-    console.log(JSON.stringify(record, null, 2));
+    const newAgent ={
+        "nom" : "cule",
+        "prenom" : "Jean",
+        "email" : "jean.cule@example.com",
+    };
+    
+    await addNewAgent(newAgent);
     } catch (e) {
     console.error(e);
     } */
 
 /* try {
-    const record = await surfaceORprice();
-    console.log(JSON.stringify(record, null, 2));
-    } catch (e) {
-    console.error(e);
-    } */
+    const RECORD_ID ="hizh4f6j1xtotly";
 
-/* try {
-    const record = await AgentID();
-    console.log(JSON.stringify(record, null, 2));
-    } catch (e) {
-    console.error(e);
-    } */
-
-/* try {
-    const record = await allMaisonsAgents('');
-    console.log(JSON.stringify(record, null, 2));
-    } catch (e) {
-    console.error(e);
-    } */
-
-/* try {
-    const record = await allMaisonsByAgentId('grlhec2g0wp03uz');
-    console.log(JSON.stringify(record, null, 2));
-    } catch (e) {
-    console.error(e);
-    } */
-
-/* try {
-    const record = await allMaisonsByAgentName('Lee');
-    console.log(JSON.stringify(record, null, 2));
-    } catch (e) {
-    console.error(e);
-    }  */
-
-/* try {
-    const record = await allMaisonsSortedAgent('Lee');
-    console.log(JSON.stringify(record, null, 2));
-    } catch (e) {
-    console.error(e);
-    }  */
-
-/*  try {
-    const record = await bySurfaceAgent('Lee');
-    console.log(JSON.stringify(record, null, 2));
-    } catch (e) {
-    console.error(e);
-    } */
-
-/* try {
-    const record = await bySurfaceAgent(600, 'grlhec2g0wp03uz');
-    console.log(JSON.stringify(record, null, 2));
+    await DeleteMaisonById(RECORD_ID);
     } catch (e) {
     console.error(e);
     } */
 
 try {
-    const record = await maisonFavoriAgent('grlhec2g0wp03uz');
-    console.log(JSON.stringify(record, null, 2));
+    const RECORD_ID ="di2c273jjfy2zct";
+    
+    await DeleteAgentById(RECORD_ID);
     } catch (e) {
     console.error(e);
     }
